d16d50060a2d8bf3d1e90fc597deceab602e41c4
vinicios-tribess <77762865+vinicios-tribess@users.noreply.github.com>
2021-02-25 03:47:23 -0300

    Versão 2
    Concluída toda a parte de cadastros. Ainda falta fazer as partes para consulta.

--- Main.py
+++ Main.py
@@ -1,21 +1,37 @@
 import sqlite3
 
-print(
-    "Digite o nome e sobrenome de cada pessoa que participará do treinamento. \n"
-    "Quando tiver terminado de digitar um nome, pressione Enter para digitar o próximo. \n"
-    "Quando tiver terminado de digitar todos os nomes, pressione Tab para prosseguir."
-)
-
-banco = sqlite3.connect('Pessoas.db')
+banco = sqlite3.connect('BancoDeDados.db')
 cursor = banco.cursor()
 
-resposta = "S"
-while resposta == "S":
-    nome = str(input("Digite um valor: "))
-    cursor.execute("CREATE TABLE IF NOT EXISTS pessoas (nomes text not null)")
-    cursor.execute(f"INSERT INTO pessoas VALUES('{nome}')")
+n_pessoas = int(input("Digite o número de pessoas que participarão do treinamento: "))
+
+print("Digite o nome e sobrenome de cada pessoa que participará do treinamento: ")
+
+for i in range (1, n_pessoas + 1):
+    nome = str(input(f"Digite o nome e sobrenome da {i}ª pessoa: "))
+    cursor.execute("CREATE TABLE IF NOT EXISTS pessoas (id_pessoa integer primary key, nome_pessoa text not null)")
+    cursor.execute(f"INSERT INTO pessoas VALUES('{i}', '{nome}')")
     banco.commit()
-    resposta = (str(input("Quer digitar mais algum nome? [S/N]"))).upper()
+
+n_salas = int(input("Digite o número de salas onde o treinamento será realizado: "))
+
+print("Digite o nome das salas e em seguida suas respectivas lotações: ")
+
+for i in range (1, n_salas + 1):
+    nome_sala = str(input(f"Digite o nome da sala {i}: "))
+    lotacao = str(input(f"Digite a lotação da sala {i}: "))
+    cursor.execute("CREATE TABLE IF NOT EXISTS salas (id_sala integer primary key, nome_sala text not null, lotacao_sala integer not null)")
+    cursor.execute(f"INSERT INTO salas VALUES('{i}', '{nome_sala}', {lotacao})")
+    banco.commit()
+
+print("Digite o nome dos espaços de café e em seguida suas respectivas lotações: ")
+
+for i in range (1, 3):
+    nome_espaco = str(input(f"Digite o nome do espaço de café {i}: "))
+    lotacao = str(input(f"Digite a lotação do espaço de café {i}: "))
+    cursor.execute("CREATE TABLE IF NOT EXISTS espacos (id_espaco integer primary key, nome_espaco text not null, lotacao_espaco integer not null)")
+    cursor.execute(f"INSERT INTO espacos VALUES('{i}', '{nome_espaco}', {lotacao})")
+    banco.commit()
 
 banco.close()
 
